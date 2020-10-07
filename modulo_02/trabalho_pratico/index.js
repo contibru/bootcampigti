b347fd3b91c832eb90d97a30f19f088d30d2f578
Bruno Conti <conti.bru@gmail.com>
2020-10-07 18:29:04 -0300

    Finalizado trabalho prático

--- modulo_02/trabalho_pratico/index.js
+++ modulo_02/trabalho_pratico/index.js
@@ -2,6 +2,7 @@ import {
 	promises as fs
 } from 'fs'
 import * as readline from "readline";
+
 init()
 
 async function init() {
@@ -21,17 +22,107 @@ async function init() {
 	});
 
 	await fs.writeFile("C:/desenv/personal/bootcamp_igti/modulo_02/trabalho_pratico/Cidades_ordenado.json", JSON.stringify(cidades))
-	GenerateJsonFromCities(estados, cidades);
+	await GenerateJsonFromCities(estados, cidades);
 
-	// const rl = readline.createInterface({
-	// 	input: process.stdin,
-	// 	output: process.stdout
-	// })
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	})
 
-	// rl.question("Informe uma sigla para contar as cidades: ", async stateAcronym => {
-	// 	console.log(await CountCitiesFromState(stateAcronym))
-	// 	rl.close()
-	// })
+	rl.question("Informe uma sigla para contar as cidades: ", async stateAcronym => {
+		console.log(await CountCitiesFromState(stateAcronym))
+		rl.close()
+	})
+
+	await LogStatesCities(estados);
+	LogCitiesWithBiggestNames(estados, cidades)
+	LogCitiesWithTiniestNames(estados, cidades)
+
+	LogCitieWithBiggestName(cidades, estados);
+	LogCitieWithTiniestName(cidades, estados);
+
+
+}
+
+function LogCitieWithTiniestName(cidades, estados) {
+	cidades.sort((a, b) => {
+		return a.Nome.length - b.Nome.length || a.Nome.localeCompare(b.Nome);
+	});
+
+	var estadoComMaiorCidade = estados.find(el => {
+		return el.ID === cidades[0].Estado
+	});
+
+	console.log(`${cidades[0].Nome} - ${estadoComMaiorCidade.Sigla}`);
+}
+
+function LogCitieWithBiggestName(cidades, estados) {
+	cidades.sort((a, b) => {
+		return b.Nome.length - a.Nome.length || a.Nome.localeCompare(b.Nome)
+	});
+
+	var estadoComMaiorCidade = estados.find(el => {
+		return el.ID === cidades[0].Estado;
+	});
+
+	console.log(`${cidades[0].Nome} - ${estadoComMaiorCidade.Sigla}`);
+}
+
+function LogCitiesWithBiggestNames(estados, cidades) {
+	var citiesWithBiggestName = [];
+	for (let i = 0; i < estados.length; i++) {
+		const estado = estados[i];
+
+		let citiesFromState = cidades.filter(cidade => {
+			return cidade.Estado === estado.ID;
+		});
+		citiesFromState.sort((a, b) => {
+			return b.Nome.length - a.Nome.length || a.Nome.localeCompare(b.Nome)
+		});
+		citiesWithBiggestName.push(`${citiesFromState[0].Nome} - ${estado.Sigla}`);
+	}
+	console.log(citiesWithBiggestName);
+}
+
+function LogCitiesWithTiniestNames(estados, cidades) {
+	var citiesWithBiggestName = [];
+	for (let i = 0; i < estados.length; i++) {
+		const estado = estados[i];
+
+		let citiesFromState = cidades.filter(cidade => {
+			return cidade.Estado === estado.ID;
+		});
+		citiesFromState.sort((a, b) => {
+			return a.Nome.length - b.Nome.length || a.Nome.localeCompare(b.Nome);
+		});
+		citiesWithBiggestName.push(`${citiesFromState[0].Nome} - ${estado.Sigla}`);
+	}
+	console.log(citiesWithBiggestName);
+}
+
+// Questão 03 e 04.
+async function LogStatesCities(estados) {
+	var estadosCount = [];
+	for (let i = 0; i < estados.length; i++) {
+		const estado = estados[i];
+		let totCities = await CountCitiesFromState(estado.Sigla);
+		estadosCount.push({
+			Sigla: estado.Sigla,
+			TotalCidades: totCities
+		});
+	}
+
+	estadosCount.sort((a, b) => {
+		return b.TotalCidades - a.TotalCidades;
+	});
+	console.log(estadosCount.slice(0, 5).map((element => {
+		return `${element.Sigla} - ${element.TotalCidades}`;
+	})));
+
+	console.log(estadosCount.reverse().slice(0, 5).map((element => {
+		return `${element.Sigla} - ${element.TotalCidades}`;
+	})).reverse());
+
 }
 
 // Questão 02.
---
+++ modulo_02/trabalho_pratico/teste.js
@@ -0,0 +1,1 @@
+console.log("Una".localeCompare("Luz"));
