7d466818b182bf44b832c77831ebaf74930b45ac
Bruno Conti <conti.bru@gmail.com>
2020-11-04 22:14:33 -0300

    Trabalho prático módulo 04

--- modulo_04/trabalho_pratico/my-bank-api/index.js
+++ modulo_04/trabalho_pratico/my-bank-api/index.js
@@ -101,22 +101,101 @@ app.route("/delete")
                 await accountsModel.count({ agencia: check.agencia }, (err, count) => {
                     res.status(200).send(count.toString())
                 })
-        
-
             }
         });
-
         if (!account)
             res.sendStatus(404)
-
-        
-
-
-
-
     })
 
+app.route("/transfer")
+    .post(async (req, res) => {
+        let transfer = req.body
+        
+        const accountOrigin = await accountsModel.findOne({
+            conta: transfer.contaOrigem
+        })
 
+        const accountDestination = await accountsModel.findOne({
+            conta: transfer.contaDestino
+        })
+
+        if (accountOrigin.balance - transfer.valor < 0) {
+            res.status(500).send("Saldo insuficiente")
+        }
+
+        if (accountOrigin.agencia != accountDestination.agencia) {
+
+            if (accountOrigin.balance - 8 - transfer.valor < 0) {
+                res.status(500).send("Saldo insuficiente")
+            }
+
+            accountOrigin.balance = accountOrigin.balance - 8
+        }
+
+        accountOrigin.balance = accountOrigin.balance - transfer.valor
+        accountDestination.balance = accountDestination.balance + transfer.valor
+
+        await accountsModel.updateOne({ conta: accountOrigin.conta }, { balance: accountOrigin.balance });
+        await accountsModel.updateOne({ conta: accountDestination.conta }, { balance: accountDestination.balance });
+
+        res.send(accountOrigin.balance.toString())
+    })
+
+    app.route("/avgbalance")
+    .get(async (req, res) => {
+        let avg = req.body
+        
+        var avgBalance = await accountsModel.aggregate([
+            {
+              '$match': {
+                'agencia': avg.agencia
+              }
+            }, {
+              '$group': {
+                '_id': '$agencia', 
+                'avg': {
+                  '$avg': '$balance'
+                }
+              }
+            }
+          ])
+          console.log(avgBalance);
+        res.send(avgBalance[0].avg.toString())
+    })
+
+    app.route("/getpoorcostumer")
+    .get(async (req, res) => {
+        let poorCostumer = req.body
+        
+        var listPoor = await accountsModel.aggregate([
+            {
+              '$sort': {
+                'balance': 1
+              }
+            }, {
+              '$limit': poorCostumer.qtdResults
+            }
+          ])
+
+        res.send(listPoor)
+    })    
+
+    app.route("/getrichcostumer")
+    .get(async (req, res) => {
+        let richCostumer = req.body
+        
+        var listRich = await accountsModel.aggregate([
+            {
+              '$sort': {
+                'balance': -1
+              }
+            }, {
+              '$limit': richCostumer.qtdResults
+            }
+          ])
+
+        res.send(listRich)
+    }) 
 
 // Tratamento de erro.
 app.use((err, req, res, next) => {
